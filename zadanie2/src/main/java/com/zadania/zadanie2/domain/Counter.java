42afeb862fd37770919dca8147a7002729f51627
Student <student@student-VirtualBox.(none)>
2012-10-28 18:36:54 +0100

    gotowe zadanie 2

---
+++ zadanie2/src/main/java/com/zadania/zadanie2/domain/Counter.java
@@ -0,0 +1,25 @@
+package com.zadania.zadanie2.domain;
+
+public class Counter {
+	private int counter=0;
+	
+	public Counter(){
+		super();
+	}
+
+	public int getCounter() {
+		return counter;
+	}
+
+	public void setCounter(int counter) {
+		this.counter = counter;
+	}
+	
+	public void incCounter() {
+		this.counter += 1;
+	}
+	
+	public void decCounter() {
+		this.counter -= 1;
+	}
+}
---
+++ zadanie2/src/main/java/com/zadania/zadanie2/domain/Log.java
@@ -0,0 +1,18 @@
+package com.zadania.zadanie2.domain;
+
+public class Log {
+	private boolean log = false;
+
+	public Log(){
+		super();
+	}
+	
+	public boolean isLog() {
+		return log;
+	}
+
+	public void setLog(boolean log) {
+		this.log = log;
+	}
+	
+}
